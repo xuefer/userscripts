b8ad686694ad0bf04a1334795106926dc324d5e7
Xuefer <xuefer@gmail.com>
2019-06-05 08:32:43 +0800

    adds m-team
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- bangumi-ep-linker.js
+++ bangumi-ep-linker.js
@@ -2,7 +2,7 @@
 // @name         bangumi ep linker
 // @name:zh-CN   Bangumi ep linker
 // @namespace    http://xuefer.win/
-// @version      0.2
+// @version      0.3
 // @description  "link episode to torrent web"
 // @description:zh-CN "链接每集到 BT 下载网站"
 // @author       Xuefer
@@ -65,15 +65,18 @@ function changeLayout() {
 	title = title[0];
 
 	var keyword = title + " S" + season.zeroPad(2) + "E" + ep.zeroPad(2);
-	var link1 = "https://thepiratebay.org/search/" + encodeURIComponent(keyword) + "/0/99/0";
-	var link2 = "https://rarbg.to/torrents.php?order=seeders&by=DESC&search=" + encodeURIComponent(keyword);
-	var link3 = "http://www.1377x.to/search/" + encodeURIComponent(keyword) + "/1/";
+	var links = {
+		'[TPB]': "https://thepiratebay.org/search/" + encodeURIComponent(keyword) + "/0/99/0",
+		'[RARBG]': "https://rarbg.to/torrents.php?order=seeders&by=DESC&search=" + encodeURIComponent(keyword),
+		'[1377x]': "https://www.1377x.to/search/" + encodeURIComponent(keyword) + "/seeders/desc/1/",
+		'[M-TEAM]': "https://tp.m-team.cc/torrents.php?search=" + encodeURIComponent(keyword) + "&sort=7&type=desc",
+	};
 
 	var titleElement = $("#columnEpA h2.title");
 	$('<br>').appendTo(titleElement);
-	$('<a>', { text: '[TPB]', href: link1, class: 'l', }).appendTo(titleElement);
-	$('<a>', { text: '[RARBG]', href: link2, class: 'l', }).appendTo(titleElement);
-	$('<a>', { text: '[1337x]', href: link3, class: 'l', }).appendTo(titleElement);
+	for (var text in links) {
+		$('<a>', { text: text, href: links[text], class: 'l', }).appendTo(titleElement);
+	}
 }
 
 changeLayout();
